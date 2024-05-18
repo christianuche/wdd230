7ed5083aff1549d5fffe6dcf2eae4bc58ea8a8a1
Christian Uche <christianuche720@gmail.com>
2024-05-18 19:03:49 +0100

    form creation

--- chamber/scripts/getDateTime.js
+++ chamber/scripts/getDateTime.js
@@ -8,33 +8,6 @@ hamburgerElement.addEventListener('click', () => {
     hamburgerElement.classList.toggle('open')
 })
 
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-const body = document.querySelector("body");
-const container = document.querySelector(".grid-container");
-
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-        body.style.background = "#000";
-		body.style.color = "#fff";
-        container.style.background = "#000";
-		container.style.color = "#fff";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-        body.style.background = "#eee";
-		body.style.color = "#000";
-        container.style.background = "#fff";
-		container.style.color = "#000";
-	}
-});
-
-
 document.addEventListener("DOMContentLoaded", function() {
     // Lazy loading images
     var lazyloadImages = document.querySelectorAll(".lazy");
