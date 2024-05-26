5d1fd4932972aa7ec55c33910af170999a47a34a
Christian Uche <christianuche720@gmail.com>
2024-05-26 17:32:41 +0100

    testing

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,25 +1,40 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// Replace '[enter your key here]' with your actual OpenWeatherMap API key
 const apiKey = '74197396bec3354ca932de15f887aaf9';
-const city = 'Abia State';
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+const lat = 49.75;
+const lon = 6.64;
+const units = 'imperial';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
-async function getWeather() {
-    try {
-        const response = await fetch(apiURL);
-        const data = await response.json();
-        displayWeather(data);
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
+// Asynchronous function to fetch weather data
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // Output the results to the console for testing
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-function displayWeather(data) {
-    const temperature = document.getElementById('temperature');
-    const description = document.getElementById('description');
-    const weatherIcon = document.getElementById('weather-icon');
-    
-    temperature.textContent = `Temperature: ${data.main.temp}°C`;
-    description.textContent = `Description: ${data.weather[0].description}`;
-    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">`;
+// Function to display the results in the HTML document
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
 }
 
-getWeather();
+// Call the apiFetch function to fetch and display the data
+apiFetch();
