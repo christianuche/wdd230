833bed4bf100ddd8774884810b854b1004a16d1b
Christian Uche <christianuche720@gmail.com>
2024-05-27 18:14:54 +0100

    another update

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,10 @@
-const baseURL = ""
-const linksURL = ""
+const baseURL = "https://christianuche.github.io/wdd230/"
+const linksURL = "https://christianuche.github.io/wdd230/data/links.json"
 
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data)
+}
+
+getLinks();
