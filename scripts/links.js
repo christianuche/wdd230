dfa492477fadd6fc32f2c93ffb02157fa070a372
Christian Uche <christianuche720@gmail.com>
2024-05-28 15:46:30 +0100

    updated the chamber

--- scripts/links.js
+++ scripts/links.js
@@ -11,32 +11,3 @@ async function getLinks() {
       console.error('Error fetching links:', error);
     }
   }
-  
-  function displayLinks(weeks) {
-    const activityLinksSection = document.getElementById('activity-links');
-    activityLinksSection.innerHTML = ''; // Clear any existing content
-  
-    weeks.forEach(week => {
-      const weekDiv = document.createElement('div');
-      weekDiv.classList.add('week');
-  
-      const weekTitle = document.createElement('h2');
-      weekTitle.textContent = week.week;
-      weekDiv.appendChild(weekTitle);
-  
-      const linksList = document.createElement('ul');
-      week.links.forEach(link => {
-        const listItem = document.createElement('li');
-        const anchor = document.createElement('a');
-        anchor.href = `${baseURL}${link.url}`;
-        anchor.textContent = link.title;
-        listItem.appendChild(anchor);
-        linksList.appendChild(listItem);
-      });
-  
-      weekDiv.appendChild(linksList);
-      activityLinksSection.appendChild(weekDiv);
-    });
-  }
-  
-  getLinks();
