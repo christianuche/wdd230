ccbfc54ba454e4b4b7eec1ab18202b959ec84886
Christian Uche <christianuche720@gmail.com>
2024-06-06 18:04:52 +0100

    just updated

--- chamber/scripts/home-members.js
+++ chamber/scripts/home-members.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://christianuche.github.io/wdd230/"
-const linksURL = `${baseURL}chamber/data/links.json`;
+const linksURL = `${baseURL}chamber/data/home-members.json`;
 
 async function fetchMembers() {
     try {
